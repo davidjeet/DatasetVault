caeb5cfdd8be5f7b49e953f59f64525549ba56b0
David Green <davidgreen24@gmail.com>
2015-12-25 17:02:55 -0500

    Adding configuration module

---
+++ DatasetVault.Common/DVConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatasetVault.Common
+{
+    public class DVConfiguration
+    {
+        private static DVConfiguration instance;
+
+        //properties go here!!!!
+
+        private DVConfiguration()
+        {
+            ////ConfigurationFileMap fileMap = new ConfigurationFileMap(file); //Path to your config file
+            ////Configuration configuration = ConfigurationManager.OpenMappedMachineConfiguration(fileMap);
+            ////string value = configuration.AppSettings.Settings["key1"].Value;
+        }
+
+        public static DVConfiguration Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DVConfiguration();
+                }
+                return instance;
+            }
+
+        }
+    }
+}
